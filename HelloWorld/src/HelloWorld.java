7817feb1e32f95c9d727797551116509975f4082
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-08 18:14:23 +0100

    Added files from the first basic lectures.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println("Hello, Kyle!");
+
+	//Assign the variable myFirstNumber a value
+	int myFirstNumber = 5;
+
+	//Access the variable and print the result
+	System.out.println(myFirstNumber);
+    }
+
+}
