ecae587ea3226d77d3a57a6e789e952a6f8b4d07
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-09 17:21:25 +0100

    Added the If Statement File

---
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("If Statements & Code Blocks\n----------------------------------------------------");
+
+	boolean gameOver = true;
+	int score = 5001;
+	int levelCompleted = 5;
+	int bonus = 100;
+
+	if (score < 5000) {
+	    System.out.println("Your Score was less than: 5000");
+	} else if (score == 5000){
+	    System.out.println("Score is 5000");
+
+	} else {
+	    System.out.println("Here !");
+
+	}
+
+	
+    }
+
+}
+ 
