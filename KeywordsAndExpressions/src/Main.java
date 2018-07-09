8bf1015f615d34ab593f0b38884b4c1b31c09691
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-09 17:24:14 +0100

    Added the Keywords & Expressions File.

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("Keywords & Expressions");
+
+
+	// This entire line is an expression
+	int score = 100;
+
+	// The 'score > 99' is the expression here
+	if (score > 99) {
+
+	    // The ' "You got the high score" ' is the expression
+	    System.out.println("You got the high score");
+
+	    // This entire line is also an expression
+	    score = 0;
+
+	}
+
+    }
+}
